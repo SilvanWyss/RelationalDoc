d51eb8beff5cb657ffe122ea4b531647c7f118bc
Nimeon <silvan.wyss@gmail.com>
2023-09-09 22:13:34 +0200

    created RelationalDocContext

---
+++ Source/ch/nolix/relationaldoc/webapplication/context/RelationalDocContext.java
@@ -0,0 +1,34 @@
+package ch.nolix.relationaldoc.webapplication.context;
+
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.relationaldocapi.webapplicationapi.contextapi.IRelationalDocContext;
+import ch.nolix.system.graphic.color.Color;
+import ch.nolix.system.graphic.image.MutableImage;
+import ch.nolix.systemapi.graphicapi.imageapi.IImage;
+import ch.nolix.techapi.relationaldocapi.dataaapterapi.IDataAdapter;
+
+public final class RelationalDocContext implements IRelationalDocContext {
+	
+	public static RelationalDocContext withDataAdapter(IDataAdapter dataAdapter) {
+		return new RelationalDocContext(dataAdapter);
+	}
+	
+	private final IDataAdapter dataAdapter;
+	
+	private RelationalDocContext(final IDataAdapter dataAdapter) {
+		
+		GlobalValidator.assertThat(dataAdapter).thatIsNamed(IDataAdapter.class).isNotNull();
+		
+		this.dataAdapter = dataAdapter;
+	}
+	
+	@Override
+	public IDataAdapter createDataAdapter() {
+		return dataAdapter.getEmptyCopy();
+	}
+	
+	@Override
+	public IImage getApplicationLogo() {
+		return MutableImage.withWidthAndHeightAndColor(100, 100, Color.DARK_GREEN);
+	}
+}
